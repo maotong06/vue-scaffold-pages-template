6490f96f3fe2cdb7561ab81862180693e14ea0d1
limaotong <limaotong@lztogether.com>
2018-04-02 22:37:28 +0800

    udpate

---
+++ template/vue-blank/vue-blank.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="vue-blank">
+    vue-blank
+  </div>
+</template>
+
+<script>
+import mixin_g from './vue-blank.g.js';
+import mixin_ex from './vue-blank.ex.js';
+export default {
+  mixins: [mixin_g, mixin_ex]
+}
+</script>
+
+<style lang="less" scoped>
+@import "./vue-blank.less";
+</style>
---
+++ template/vue-simple-blank/vue-simple-blank.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="vue-simple-blank">
+    vue-simple-blank
+  </div>
+</template>
+
+<script>
+import mixin_g from './vue-simple-blank.g.js';
+export default {
+  mixins: [mixin_g]
+}
+</script>
+
+<style lang="less" scoped>
+.vue-simple-blank{
+
+}
+</style>
