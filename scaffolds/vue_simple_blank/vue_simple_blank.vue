9171c2a94eb1a6f68789c22774702bb84c3105a6
maotong06 <limaotong>
2018-04-09 10:03:56 +0800

    update name

--- scaffolds/vue_simple_blank/vue_simple_blank.vue
+++ scaffolds/vue_simple_blank/vue_simple_blank.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="vue-simple-blank">
-    vue-simple-blank
+  <div class="vue_simple_blank">
+    vue_simple_blank
   </div>
 </template>
 
 <script>
-import mixin_g from './vue-simple-blank.g.js';
+import mixin_g from './vue_simple_blank.g.js';
 export default {
   mixins: [mixin_g]
 }
 </script>
 
 <style lang="less" scoped>
-.vue-simple-blank{
+.vue_simple_blank{
 
 }
 </style>
