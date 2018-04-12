f4f15d3589e72a45f51798246a5e2ccd759a21c4
maotong06 <limaotong>
2018-04-12 13:15:40 +0800

    udpate [%%]

---
+++ scaffolds/vue_blank/[%=code%].vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="[%=code%]">
+    {{[%=code%]}}
+  </div>
+</template>
+
+<script>
+import mixin_g from './[%=code%].g.js';
+import mixin_ex from './[%=code%].ex.js';
+export default {
+  mixins: [mixin_g, mixin_ex]
+}
+</script>
+
+<style lang="less" src="./[%=code%].less" scoped></style>
